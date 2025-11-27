c970d36caecea9495449abc19ee9256d13efb7a4
timoalireza <timoalireza@users.noreply.github.com>
2025-11-27 18:39:04 +0100

    chore: Remove RevenueCat fields migration from database schema
    - Delete the SQL migration file that added RevenueCat customer and subscription ID fields to the subscriptions table, as it is no longer needed.

--- apps/api/drizzle/0004_add_revenuecat_fields.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add RevenueCat fields to subscriptions table
--- This migration adds support for RevenueCat subscription tracking
-
--- Add RevenueCat customer ID (maps to Clerk user ID)
-ALTER TABLE "subscriptions" ADD COLUMN IF NOT EXISTS "revenuecat_customer_id" text;
-
--- Add RevenueCat subscription/transaction ID
-ALTER TABLE "subscriptions" ADD COLUMN IF NOT EXISTS "revenuecat_subscription_id" text;
-
